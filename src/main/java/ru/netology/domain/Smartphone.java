0cc51f8e8c455197aad7a03e49b13f24c4027fea
Aleksandr Olenin <aleksandr.olenin@mail.ru>
2020-04-06 18:29:55 +0300

    Добавлен Smartphone

---
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -0,0 +1,19 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Smartphone extends Product {
+    private String producer;
+
+    public Smartphone(int id, String name, int price, String producer) {
+        super(id, name, price);
+        this.producer = producer;
+    }
+}
