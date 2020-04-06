955eb5578e6653c6815319046dfa1a802c32647a
Aleksandr Olenin <aleksandr.olenin@mail.ru>
2020-04-06 19:27:06 +0300

    Добавлен ProductManager

---
+++ src/main/java/ru/netology/domain/ProductManager.java
@@ -0,0 +1,45 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ProductManager {
+    private Repository repo;
+
+
+    public void addProduct(Product item) {
+        repo.save(item);
+    }
+
+    public Product[] searchBy(String text) {
+        Product[] newList = new Product[0];
+        for (Product item : repo.getAll()) {
+            if (matches(item, text)) {
+                Product[] tmp = new Product[newList.length + 1];
+                System.arraycopy(newList, 0, tmp, 0, newList.length);
+                tmp[tmp.length - 1] = item;//
+                newList = tmp;
+            }
+        }
+        return newList;
+    }
+
+    private boolean matches(Product product, String search) {
+        if (product instanceof Book) {
+            Book book = (Book) product;
+            if (book.getName().equalsIgnoreCase(search)) {
+                return true;
+            }
+            return book.getAuthor().equalsIgnoreCase(search);
+        } else if (product instanceof Smartphone) {
+            Smartphone phone = (Smartphone) product;
+            if (phone.getProducer().equalsIgnoreCase(search)) {
+                return true;
+            }
+            return phone.getName().equalsIgnoreCase(search);
+        }
+        return false;
+    }
+}
