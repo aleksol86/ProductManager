109f22e6837b3e93eb9e9ba71ba242df0b7c0baf
Aleksandr Olenin <aleksandr.olenin@mail.ru>
2020-04-06 21:59:36 +0300

    Обновлен ProductManager

--- src/main/java/ru/netology/domain/ProductManager.java
+++ src/main/java/ru/netology/domain/ProductManager.java
@@ -15,30 +15,13 @@ public class ProductManager {
     public Product[] searchBy(String text) {
         Product[] newList = new Product[0];
         for (Product item : repo.getAll()) {
-            if (matches(item, text)) {
+            if (item.matches(text)) {
                 Product[] tmp = new Product[newList.length + 1];
                 System.arraycopy(newList, 0, tmp, 0, newList.length);
-                tmp[tmp.length - 1] = item;//
+                tmp[tmp.length - 1] = item;
                 newList = tmp;
             }
         }
         return newList;
-    }
-
-    private boolean matches(Product product, String search) {
-        if (product instanceof Book) {
-            Book book = (Book) product;
-            if (book.getName().equalsIgnoreCase(search)) {
-                return true;
-            }
-            return book.getAuthor().equalsIgnoreCase(search);
-        } else if (product instanceof Smartphone) {
-            Smartphone phone = (Smartphone) product;
-            if (phone.getProducer().equalsIgnoreCase(search)) {
-                return true;
-            }
-            return phone.getName().equalsIgnoreCase(search);
-        }
-        return false;
     }
 }
