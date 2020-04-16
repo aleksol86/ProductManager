b31cb9cd72d3995a9f1bfb9336fdf421e9f07a60
Aleksandr Olenin <aleksandr.olenin@mail.ru>
2020-04-16 15:42:29 +0300

    Обновлены классы ProductManagerTest и Repository

--- src/main/java/ru/netology/repository/Repository.java
+++ src/main/java/ru/netology/repository/Repository.java
@@ -1,16 +1,9 @@
 package ru.netology.repository;
 
-import ru.netology.domain.Book;
 import ru.netology.domain.Product;
-import ru.netology.domain.Smartphone;
 
 public class Repository {
-    Product[] repo = {new Book(1, "Book 1", 200, "Author 1"),
-            new Book(2, "Book 2", 100, "Author 2"),
-            new Book(3, "Book 3", 100, "Author 3"),
-            new Smartphone(1, "S20", 300, "Samsung"),
-            new Smartphone(2, "P40", 300, "Huawei"),
-            new Smartphone(3, "iPhone 11", 50, "Apple")};
+    private Product[] repo = new Product[0];
 
     public void save(Product item) {
         Product[] newRepo = new Product[repo.length + 1];
--- src/test/java/ru/netology/domain/ProductManagerTest.java
+++ src/test/java/ru/netology/domain/ProductManagerTest.java
@@ -1,5 +1,6 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.manager.ProductManager;
 import ru.netology.repository.Repository;
@@ -7,39 +8,51 @@ import ru.netology.repository.Repository;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class ProductManagerTest {
+    private Book book1 = new Book(1, "Book 1", 200, "Author 1");
+    private Book book2 = new Book(2, "Book 2", 100, "Author 2");
+    private Book book3 = new Book(3, "Book 3", 200, "Author 3");
+    private Smartphone smartphone1 = new Smartphone(1, "S20", 300, "Samsung");
+    private Smartphone smartphone2 = new Smartphone(2, "P40", 300, "Huawei");
+    private Smartphone smartphone3 = new Smartphone(3, "iPhone 11", 50, "Apple");
+    private Repository repository;
+    private ProductManager manager;
+
+    @BeforeEach
+    void setUp() {
+        repository = new Repository();
+        manager = new ProductManager(repository);
+        manager.addProduct(book1);
+        manager.addProduct(book2);
+        manager.addProduct(book3);
+        manager.addProduct(smartphone1);
+        manager.addProduct(smartphone2);
+        manager.addProduct(smartphone3);
+    }
 
     @Test
     void shouldSearchByAuthor() {
-        Repository repository = new Repository();
-        ProductManager manager = new ProductManager(repository);
-        Product[] expected = {new Book(2, "Book 2", 100, "Author 2")};
+        Product[] expected = {book2};
         Product[] actual = manager.searchBy("Author 2");
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void shouldSearchByName() {
-        Repository repository = new Repository();
-        ProductManager manager = new ProductManager(repository);
-        Product[] expected = {new Book(3, "Book 3", 100, "Author 3")};
+        Product[] expected = {book3};
         Product[] actual = manager.searchBy("Book 3");
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void shouldSearchByProducer() {
-        Repository repository = new Repository();
-        ProductManager manager = new ProductManager(repository);
-        Product[] expected = {new Smartphone(1, "S20", 300, "Samsung")};
+        Product[] expected = {smartphone1};
         Product[] actual = manager.searchBy("Samsung");
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void shouldSearchByPhoneName() {
-        Repository repository = new Repository();
-        ProductManager manager = new ProductManager(repository);
-        Product[] expected = {new Smartphone(1, "S20", 300, "Samsung")};
+        Product[] expected = {smartphone1};
         Product[] actual = manager.searchBy("S20");
         assertArrayEquals(expected, actual);
     }
